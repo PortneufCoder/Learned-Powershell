13ff2a8ebb5b149efea63a7f4cc335f4587d4c08
Ajani <Victor.Ajani@ia.ca>
2018-10-29 15:03:18 -0400

    powershell practice

---
+++ powershell_Commands.ps1
@@ -0,0 +1,18 @@
+﻿Get-Command
+
+# in the current directory, get files with a size less than 100kb
+Get-ChildItem | Where-Object { $_.Length -gt 100kb }
+
+$victor = "Born in Africa"
+$victor #call the value of victor
+
+$country = "specifically nigeria"
+
+$address = $victor + $country
+
+$address
+
+# Long version og $var = 123
+New-Variable -Name var -Value 123
+$var
+
